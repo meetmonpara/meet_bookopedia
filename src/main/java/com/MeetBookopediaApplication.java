98701585f64fad757b24489b6d778293f0499e37
meetmonpara <meetmonpara@gmail.com>
2022-02-08 09:55:16 +0530

    init

---
+++ src/main/java/com/MeetBookopediaApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetBookopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetBookopediaApplication.class, args);
+	}
+
+}
